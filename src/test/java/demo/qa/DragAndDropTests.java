09a0cb01f903401e036cea5aada00ec5f5f99c6f
Anya <crshzy@gmail.com>
2024-07-28 12:16:28 +0200

    add new HW

---
+++ src/test/java/demo/qa/DragAndDropTests.java
@@ -0,0 +1,43 @@
+package demo.qa;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
+public class DragAndDropTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+    }
+
+    @Test
+    void dragAndDropActionTest() {
+        open("/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+
+        actions().moveToElement($("#column-a")).clickAndHold()
+                .moveByOffset(200, 0).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    void dragAndDropMethodTest() {
+        open("/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
