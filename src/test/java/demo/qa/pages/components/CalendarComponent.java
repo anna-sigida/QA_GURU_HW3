ef7c3b506ff61e92e790a0d61c065d32fd185311
Anna Sigida <sigida_a_k@ntik.ru>
2024-09-12 09:37:26 +0300

    fix CalendarComponent

--- src/test/java/demo/qa/pages/components/CalendarComponent.java
+++ src/test/java/demo/qa/pages/components/CalendarComponent.java
@@ -1,7 +1,9 @@
 package demo.qa.pages.components;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class CalendarComponent {
     public void setDate(String year, String month, String day) {
@@ -9,6 +11,6 @@ public class CalendarComponent {
         $(".react-datepicker__year-select").$(byText(year)).click();
         $(".react-datepicker__month-dropdown-container--select").click();
         $(".react-datepicker__month-select").$(byText(month)).click();
-        $(".react-datepicker__month").$(byText(day)).click();
+        $$(".react-datepicker__day:not(.react-datepicker__day--outside-month)").findBy(text(day)).click();
     }
 }
