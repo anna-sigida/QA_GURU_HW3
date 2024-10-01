01cdae2f939cfb6d0f855357978590e0b7789553
Anna Sigida <sigida_a_k@ntik.ru>
2024-10-01 17:38:47 +0300

    add selenoid

--- src/test/java/demo/qa/BaseTest.java
+++ src/test/java/demo/qa/BaseTest.java
@@ -9,6 +9,7 @@ public class BaseTest {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         //Configuration.holdBrowserOpen = true;
     }
 }
